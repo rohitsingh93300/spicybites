edde2fab7ad0b0c400c0d39b96060071828f4886
rohitsingh93300 <rohitsingh93300@gmail.com>
2024-05-15 10:00:02 +0530

    bug fixing

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,12 +4,12 @@ import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcryptjs"
 import mongoose from "mongoose";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import clientPromise from "@/lib/mongoConnect"
+// import { MongoDBAdapter } from "@auth/mongodb-adapter"
+// import clientPromise from "@/lib/mongoConnect"
 
 const handler = NextAuth({
   secret:process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
+  // adapter: MongoDBAdapter(clientPromise),  
     providers: [
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
